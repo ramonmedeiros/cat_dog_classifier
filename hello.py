34d6153c0625b20455782b5268d1dc2b06c6dc74
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-02-03 17:29:44 +0100

    Done :p

---
+++ hello.py
@@ -0,0 +1,190 @@
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# DATA FROM https://cdn.freecodecamp.org/project-data/cats-and-dogs/cats_and_dogs.zip
+PATH = 'cats_and_dogs'
+
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
+test_dir = os.path.join(PATH, 'test')
+
+def plotImages(images_arr, probabilities = False):
+    fig, axes = plt.subplots(len(images_arr), 1, figsize=(5,len(images_arr) * 3))
+    if probabilities is False:
+      for img, ax in zip( images_arr, axes):
+          ax.imshow(img)
+          ax.axis('off')
+    else:
+      for img, probability, ax in zip( images_arr, probabilities, axes):
+          ax.imshow(img)
+          ax.axis('off')
+          if probability > 0.5:
+              ax.set_title("%.2f" % (probability*100) + "% dog")
+          else:
+              ax.set_title("%.2f" % ((1-probability)*100) + "% cat")
+    plt.show()
+
+# Get number of files in each directory. The train and validation directories
+# each have the subdirecories "dogs" and "cats".
+total_train = sum([len(files) for r, d, files in os.walk(train_dir)])
+total_val = sum([len(files) for r, d, files in os.walk(validation_dir)])
+total_test = len(os.listdir(test_dir))
+
+# Variables for pre-processing and training.
+batch_size = 128
+epochs = 16
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+
+## LOAD DATA
+# this part group the images in a dataset using ImageDataGenerator
+train_image_generator      = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+test_image_generator       = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(
+                    train_dir,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=batch_size,
+                    class_mode='binary')
+
+val_data_gen   = validation_image_generator.flow_from_directory(
+                    validation_dir,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=batch_size,
+                    class_mode='binary')
+
+test_data_gen  = test_image_generator.flow_from_directory(
+                    PATH,
+                    target_size=(IMG_HEIGHT, IMG_WIDTH),
+                    batch_size=batch_size,
+                    classes=['test'],
+                    shuffle=False)
+
+sample_training_images, _ = next(train_data_gen)
+#plotImages(sample_training_images[:5])
+
+
+# DATA AUGMENTATION
+# apply random transformations
+train_image_generator = ImageDataGenerator(
+                            rescale=1./255,
+                            horizontal_flip=True,
+                            rotation_range=20,
+                            zoom_range=0.15,
+                            width_shift_range=0.2,
+                            height_shift_range=0.2,
+                            shear_range=0.15,
+                            fill_mode="nearest")
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                     directory=train_dir,
+                                                     target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                     class_mode='binary')
+
+augmented_images = [train_data_gen[0][0][0] for i in range(5)]
+#plotImages(augmented_images)
+
+model = Sequential()
+
+# Convolutions
+model.add(Input(shape=(IMG_HEIGHT, IMG_WIDTH, 3)))
+model.add(Conv2D(32, (3,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3,3)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Dense layers
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# train and save the model
+
+# set callback and path
+#checkpoint_path = "training_1/cp.ckpt"
+#checkpoint_dir = os.path.dirname(checkpoint_path)
+#
+## Create a callback that saves the model's weights
+#cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+#                                                 save_weights_only=True,
+#                                                 verbose=1)
+#
+## if model exists, load it: otherwise train
+#if os.path.exists(checkpoint_path):
+#    pass
+#    #model.load_weights(checkpoint_path)
+#else:
+
+# Optimizer
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_data_gen,
+          steps_per_epoch=int(round(total_train / batch_size)),
+          epochs=epochs,
+          validation_data=val_data_gen,
+          validation_steps=int(round(total_val / batch_size))
+          )
+
+
+
+# check accuracy
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+# check with test data
+probabilities = model.predict(test_data_gen).flatten()
+
+answers =  [1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0,
+            1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0,
+            1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+            1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 
+            0, 0, 0, 0, 0, 0]
+
+correct = 0
+
+for probability, answer in zip(probabilities, answers):
+  if round(probability) == answer:
+    correct +=1
+
+percentage_identified = (correct / len(answers))
+
+passed_challenge = percentage_identified > 0.63
+
+print(f"Your model correctly identified {round(percentage_identified, 2)}% of the images of cats and dogs.")
+
+if passed_challenge:
+  print("You passed the challenge!")
+else:
+  print("You haven't passed yet. Your model should identify at least 63% of the images. Keep trying. You will get it!")
