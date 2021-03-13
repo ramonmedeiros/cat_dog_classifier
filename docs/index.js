09cbe17dc5731ae9f61d931b636977d26b47378d
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-03-13 12:52:42 +0100

    Wip

--- docs/index.js
+++ docs/index.js
@@ -1,29 +1,43 @@
+const WIDTH=150
+const HEIGHT=150
+
 
 async function previewFile() {
-  var input = document.getElementById("myFile");
-  var reader = new FileReader();
-  var img1 = document.getElementById("animal");
-  var predicted = document.getElementById("predicted");
-  var model = await tf.loadGraphModel("https://ramonmedeiros.github.io/cat_dog_classifier/web_model/model.json");
-  
-  reader.onload = function(e) {  
-    img1.src= e.target.result;
-  }
-  
-  img1.onload = function() { 
-    const imageTensor = tf.browser.fromPixels(img1);  
-    const smalImg = tf.image.resizeBilinear(imageTensor, [150, 150]);
-    const resized = tf.cast(smalImg, 'float32');
-    const t4d = tf.tensor4d(Array.from(resized.dataSync()),[1,150,150,3])
+  var input = document.getElementById("myFile")
+  var reader = new FileReader()
+  var img1 = document.getElementById("animal")
+  var predicted = document.getElementById("predicted")
+  var model = await tf.loadGraphModel("https://ramonmedeiros.github.io/cat_dog_classifier/web_model/model.json")
+  var resized = document.getElementById("resized")
+  var canvas = document.getElementById("canva")
 
-    //const preprocessedInput = imageTensor.expandDims();
-      
-    const prediction = model.predict(t4d);
-    predicted.innerHTML = prediction.shape;
-    console.log(prediction);
+
+  reader.onload = function(e) {
+    img1.src= e.target.result
+  }
+
+  img1.onload = function() {
+    // set size proportional to image
+    oc = canvas.getContext('2d');
+    oc.width = WIDTH;
+    oc.height = HEIGHT;
+    oc.drawImage(img1, 0, 0, img1.width, img1.height, 0, 0, canvas.width, canvas.height);
+
+    resized.width = WIDTH
+    resized.height = HEIGHT
+    resized.src = canvas.toDataURL("image/jpeg")
+    
+
+    var imageTensor = tf.browser.fromPixels(resized).toFloat();
+    //imageTensor = imageTensor.expandDims()
+    imageTensor = tf.reshape(imageTensor, [-1, WIDTH, HEIGHT, 3],'resize');
+    var prediction = model.predict(imageTensor).dataSync()
+    console.log(prediction)
+    predicted.innerHTML = prediction
   }
 
   if (input.files && input.files[0]) {
-    reader.readAsDataURL(input.files[0]);
-  } 
+    predicted.innerHTML = "Loading .."
+    reader.readAsDataURL(input.files[0])
+  }
 }
